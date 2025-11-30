dfe4a350b7786f84d299cc44a2b8a6b854fb5905
iparigoel <pggoel1334@gmail.com>
2025-11-30 13:31:34 +0530

    Fix vercel deployment

--- server.js
+++ server.js
@@ -20,25 +20,38 @@ app.use(
 
 // connectDB();
 
-let isConnected = false;
-async function connectToMongoDB(){
-  try{
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    isConnected = true;
+// let isConnected = false;
+// async function connectToMongoDB(){
+//   try{
+//     await mongoose.connect(process.env.MONGO_URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     isConnected = true;
+//     console.log("MongoDB connected successfully");
+//   }catch(error){
+//     console.error("MongoDB connection failed:", error);
+//   }
+// }
+// //add middleware
+// app.use((req, res, next) => {
+//   if(!isConnected){
+//     connectToMongoDB();
+//   }next();
+// });
+
+async function connectToMongoDB() {
+  if (isConnected) return;
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URL);
+    isConnected = db.connections[0].readyState;
     console.log("MongoDB connected successfully");
-  }catch(error){
+  } catch (error) {
     console.error("MongoDB connection failed:", error);
   }
 }
-//add middleware
-app.use((req, res, next) => {
-  if(!isConnected){
-    connectToMongoDB();
-  }next();
-});
+
 
 //Server uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
@@ -60,4 +73,8 @@ app.use((err, req, res, next) => {
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
 
-module.exports = app;
+// module.exports = app;
+module.exports = async (req, res) => {
+  await connectToMongoDB();
+  app(req, res);
+};
